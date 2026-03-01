7f15b0fcccb5af0f806f4f0945272a4a76b3e1a3
Farhan-Sharief <goodsgadi@gmail.com>
2026-03-01 21:29:22 +0530

    dahsboard

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,14 +1,22 @@
-import Sidebar from "./Sidebar";
-import Navbar from "./Navbar";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 export default function Layout() {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <div className="flex h-screen bg-gray-900 text-white">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Navbar />
-        <div className="p-6 overflow-y-auto">
+    <div className="min-h-screen bg-gradient-to-br from-[#0f172a] to-[#1e293b]">
+
+      <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
+
+      {/* Main Content */}
+      <div
+        className={`transition-all duration-300 ${
+          collapsed ? "ml-20" : "ml-64"
+        }`}
+      >
+        <div className="p-6">
           <Outlet />
         </div>
       </div>
